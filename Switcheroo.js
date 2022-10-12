8286479f6dfcb9241f29a162f08ff1437b08fcb9
Gastón Otero <48187361+GastonOtero@users.noreply.github.com>
2022-10-12 15:57:13 -0300

    Create Switcheroo.js

---
+++ Switcheroo.js
@@ -0,0 +1,16 @@
+/*
+DESCRIPTION:
+
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b 
+(change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+*/
+
+// My solution:
+
+const switcheroo = x =>
+x.replace(/[ab]/g, i => i === "a" ? i = "b" : i ="a");
