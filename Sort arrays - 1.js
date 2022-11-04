b890f3030c57dc4bfa8b2d9fea400eb71d239a0e
Gastón Otero <48187361+GastonOtero@users.noreply.github.com>
2022-11-04 14:05:34 -0300

    Create Sort arrays -1.js

---
+++ Sort arrays - 1.js
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+
+Just a simple sorting usage. Create a function that returns the elements of the input-array / 
+list sorted in lexicographical order.
+*/
+
+// My solution:
+
+sortme = function( names ){
+    return names.sort();
+  }
+
