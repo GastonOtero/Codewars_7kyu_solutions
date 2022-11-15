69b2f9bddcf98cfaec7025253bdfce41683ef131
Gastón Otero <48187361+GastonOtero@users.noreply.github.com>
2022-11-15 13:56:31 -0300

    Create Sum of all arguments.js

---
+++ Sum of all arguments.js
@@ -0,0 +1,17 @@
+/*
+DESCRIPTION:
+
+Write a function that finds the sum of all its arguments.
+
+eg:
+
+sum(1, 2, 3) // => 6
+sum(8, 2) // => 10
+sum(1, 2, 3, 4, 5) // => 15
+*/
+
+// My solution:
+
+function sum() {
+    return Array.from(arguments).reduce((a, b) => a + b);
+  }
